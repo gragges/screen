9d978bba9f23e0e03e288f5203bc9e0eb586d063
gragges <deskanataras@gmail.com>
2025-07-17 13:17:30 +0200

    second

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const { screenshot } = require('./screenshot');
+const path = require('path');
+const fs = require('fs');
+const { v4: uuid } = require('uuid');
+const port = process.env.PORT || 8080;
+
+const puppeteer = require('puppeteer');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+  scrape().then((data) => {
+    console.log(data);
+    res.send(data);
+  }).catch((error) => {
+    console.error('Error during scraping:', error);
+    res.status(500).send('Error during scraping');
+  });
+})
+app.get('/screenshot', (req, res) => {
+    screenshot(req, res);
+})
+app.get('/gra', (req, res) => {
+    res.send('Hello from gra!');
+  })
+
+app.listen(port, () => {
+  console.log('Server is running on ');
+});
+
+const scrape = async ()=>{
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const page2=await browser.newPage();
+
+    const url="https://www.bankier.pl/gielda/notowania/akcje"
+    const url2="https://www.bankier.pl/gielda/notowania/new-connect"
+
+    await page.goto(url);
+    await page2.goto(url2)
+
+
+    //const title=await page.title()
+    //console.log(`To jest tytul: ${title}`)
+    const dane = await page.evaluate(()=>{
+        const zmienna = document.querySelectorAll('.colWalor.textNowrap');
+        //const zmienna = document.querySelectorAll('.colLiczbaTransakcji');
+        
+        return Array.from(zmienna).map((walor)=>{
+            const tytul=walor.innerText
+            //const transakcje=walor.getElementsByTagName('td').querySelectorAll('.colLiczbaTransakcji').innerText;
+            return tytul
+               
+            
+        })
+    
+});}
---
+++ screenshot.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+const { v4: uuid } = require('uuid');
+const fs = require('fs');
+// Main screenshot function
+const screenshot = async (req, res) => {
+    const url = req.query.url;
+    if (!url) {
+        return res.status(400).json({ message: 'URL is required' });
+    }
+    let browser;
+    try {
+        // Launch Puppeteer with specific Chrome executable path and options
+        browser = await puppeteer.launch({
+            ignoreHTTPSErrors: true,
+            executablePath: process.env.CHROME_PATH || '/opt/bin/chromium',
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
+        const url = "https://www.bankier.pl/gielda/notowania/akcje";
+        const page = await browser.newPage();
+        await page.goto(url, { waitUntil: 'networkidle2' });
+        await page.setViewport({ width: 1920, height: 1080 });
+        const fileName = `${uuid()}.png`;
+        const screenshotPath = path.join(__dirname, '..', 'public', fileName);
+        await page.screenshot({ path: screenshotPath });
+        res.json({ screenshotPath: `/image/${fileName}` });
+    } catch (err) {
+        console.error('Error capturing screenshot:', err);
+        res.status(500).json({ error: 'Failed to capture screenshot' });
+    } finally {
+        if (browser) await browser.close();
+    }
+};
+module.exports = screenshot;
